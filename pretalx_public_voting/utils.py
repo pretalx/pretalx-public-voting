aa5b32104174da13920d08285b268f7088211b4f
Tobias Kunze <r@rixx.de>
2020-04-22 04:22:16 +0200

    Provide signing utilities
    The signing process was suggested and first implemented by Volker
    Mische, @vmx.

---
+++ pretalx_public_voting/utils.py
@@ -0,0 +1,21 @@
+from contextlib import suppress
+from hashlib import blake2b
+
+from django.core import signing
+
+
+def hash_email(email, event):
+    return blake2b(
+        email.encode("utf-8"), salt=event.slug.encode("utf-8"), digest_size=16,
+    ).hexdigest()
+
+
+def event_sign(data, event):
+    signer = signing.Signer(salt=event.slug)
+    return signer.sign(data)
+
+
+def event_unsign(data, event):
+    signer = signing.Signer(salt=event.slug)
+    with suppress(signing.BadSignature):
+        return signer.unsign(data)
