b6b76cfb8185c04a71af0cec11408c976e9fb4de
Tobias Kunze <r@rixx.de>
2021-12-07 00:04:19 +0100

    Release v0.2.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {"build": CustomBuild}
 
 setup(
     name="pretalx-public-voting",
-    version="0.2.0",
+    version="0.2.1",
     description="A public voting plugin for pretalx",
     long_description=long_description,
     url="https://github.com/pretalx/pretalx-public-voting",
