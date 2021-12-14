226f9aa790e25834731695316782eb28eee09564
Tobias Kunze <r@rixx.de>
2021-12-14 12:54:09 +0100

    Update code style

--- pretalx_public_voting/models.py
+++ pretalx_public_voting/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_scopes import ScopedManager
 from i18nfield.strings import LazyI18nString
-
 from pretalx.common.models.settings import hierarkey
 
 hierarkey.add_default("public_voting_start", None, dt.datetime)
