d3680ab71090cb5ccd7caf251635f743998c9763
Tobias Kunze <r@rixx.de>
2023-11-02 17:24:00 +0100

    Switch to pyproject.toml

--- pretalx_public_voting/apps.py
+++ pretalx_public_voting/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy
 
+from . import __version__
+
 
 class PluginApp(AppConfig):
     name = "pretalx_public_voting"
@@ -11,7 +13,7 @@ class PluginApp(AppConfig):
         author = "Tobias Kunze"
         description = gettext_lazy("A public voting plugin for pretalx")
         visible = True
-        version = "1.4.0"
+        version = __version__
         category = "FEATURE"
 
     def ready(self):
