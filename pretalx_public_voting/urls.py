05d8a5260894e22ed73f92108616893ccc72fd02
Tobias Kunze <r@rixx.de>
2023-11-14 12:50:30 +0100

    Use SLUG_REGEX for urls

--- pretalx_public_voting/urls.py
+++ pretalx_public_voting/urls.py
@@ -1,26 +1,26 @@
 from django.urls import re_path
-from pretalx.event.models.event import SLUG_CHARS
+from pretalx.event.models.event import SLUG_REGEX
 
 from . import views
 
 urlpatterns = [
     re_path(
-        rf"^orga/event/(?P<event>[{SLUG_CHARS}]+)/settings/p/public_voting/$",
+        rf"^orga/event/(?P<event>{SLUG_REGEX})/settings/p/public_voting/$",
         views.PublicVotingSettingsView.as_view(),
         name="settings",
     ),
     re_path(
-        f"^(?P<event>[{SLUG_CHARS}]+)/p/voting/signup/$",
+        f"^(?P<event>{SLUG_REGEX})/p/voting/signup/$",
         views.SignupView.as_view(),
         name="signup",
     ),
     re_path(
-        f"^(?P<event>[{SLUG_CHARS}]+)/p/voting/thanks/$",
+        f"^(?P<event>{SLUG_REGEX})/p/voting/thanks/$",
         views.ThanksView.as_view(),
         name="thanks",
     ),
     re_path(
-        f"^(?P<event>[{SLUG_CHARS}]+)/p/voting/talks/(?P<signed_user>[^/]+)/$",
+        f"^(?P<event>{SLUG_REGEX})/p/voting/talks/(?P<signed_user>[^/]+)/$",
         views.SubmissionListView.as_view(),
         name="talks",
     ),
