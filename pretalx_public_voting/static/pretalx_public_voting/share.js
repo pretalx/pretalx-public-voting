4de75727229006a798842ca85afc2da610241eb9
tjarbo <16938041+tjarbo@users.noreply.github.com>
2025-05-18 11:21:18 +0000

    allow users to use/fallback to the brower's "copy link" feature in case of JS issues

--- pretalx_public_voting/static/pretalx_public_voting/share.js
+++ pretalx_public_voting/static/pretalx_public_voting/share.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const shareButtons = document.querySelectorAll("a[data-pretalx-voting-selector='share']")
-  const linkAttributeName = "data-pretalx-voting-share-url"
   const successTextAttributeName = "data-pretalx-voting-copied-successful-text"
 
   shareButtons.forEach((button) => {
     const resetHTML = button.innerHTML; // Ensures that original text is always restored even if button is pressed multiple times.
     button.addEventListener('click', async (event) => {
       event.preventDefault();
-      const path = button.attributes.getNamedItem(linkAttributeName).value;
-      const successText = button.attributes.getNamedItem(successTextAttributeName).value;
-      if (path === null) return console.error('Share button clicked but no URL found.', { button });
-      const link = window.location.origin + path;
+      const votingPath = button.getAttribute("href");
+      const successText = button.getAttribute(successTextAttributeName);
+      if (votingPath === null) return console.error('Share button clicked but no URL found.', { button });
+      const link = window.location.origin + votingPath;
       navigator.clipboard.writeText(link)
         .then(() =>  button.textContent = successText ?? 'Copied!') // Falling back to english translation just in case.
         .catch((error) => console.error('Failed to copy url to clipboard', { link, error, button }))
